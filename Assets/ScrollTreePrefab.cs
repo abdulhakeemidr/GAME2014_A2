c92b0c5245ac4c84e6c4d2b2f12a13a8a15b0988
abdulhakeemidr <56049093+abdulhakeemidr@users.noreply.github.com>
2021-12-12 18:12:39 -0500

    Implemented Tree Scrolling Script

--- Assets/ScrollTreePrefab.cs
+++ Assets/ScrollTreePrefab.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class ScrollTreePrefab : MonoBehaviour
 {
+    public float verticalSpeed = 2f;
+    public float verticalBoundary = 8f;
+
     void Update()
     {
-        
+        transform.position -= new Vector3(0.0f, verticalSpeed) * Time.deltaTime;
+        CheckBounds();
+    }
+
+    private void CheckBounds()
+    {
+        if(transform.position.y <= -verticalBoundary)
+        {
+            transform.position = new Vector3(transform.position.x, verticalBoundary);
+        }
     }
 }
